8f01ca2f76870dd4b87751dfb6aad01e5b039d32
takaeight <tmatsukawa123@gmail.com>
2026-03-01 06:33:52 +0900

    fix(config): adjust basePath for github pages

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   // GitHub Pages用のリポジトリ名を設定（画像参照：/world-asd-news/配下に配置されるため）
-  basePath: '/world-asd-news',
+  basePath: process.env.NODE_ENV === 'production' ? '/world-asd-news' : '',
   // static exportに伴う画像の最適化OFF（場合によるが今回は安全のため）
   images: {
     unoptimized: true,
